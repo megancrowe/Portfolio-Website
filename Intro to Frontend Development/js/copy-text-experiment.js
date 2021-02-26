c8313cab113f610a8b0bf9bad41ed4938708f413
megancrowe <78937020+megancrowe@users.noreply.github.com>
2021-02-26 15:42:12 -0700

    Ammend JS file to include credit and update code
    Added credit to copy-text-experiment.js
    Updated IDs to properly link copy-text-experiment.js to contact.html

--- Intro to Frontend Development/js/copy-text-experiment.js
+++ Intro to Frontend Development/js/copy-text-experiment.js
@@ -1,6 +1,7 @@
-    function myFunction() {
+/* Code credit to: https://www.w3schools.com/howto/howto_js_copy_clipboard.asp */
+function myFunction() {
       /* Get the text field */
-        var copyText = document.getElementById("my-skype-name");
+        var copyText = document.getElementById("my-skype-name-text-box");
       /* Select the text field */
         copyText.select();
         copyText.setSelectionRange(0, 99999); /*For mobile devices*/
